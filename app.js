614232b736b7ef351cc766a175287a17c753e4fd
Santosh Gautam <santoshgautam@Santoshs-MBP.fios-router.home>
2019-04-06 14:02:35 -0400

    add console log

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ console.log(catme());
 
 console.log("******************************************************************************************");
 console.log(faker.address.streetName());
-console.log(faker.name.findName());
+console.log(faker.name.lastName());
 console.log("******************************************************************************************");
 
 console.log(jokes());
 console.log("******************************************************************************************");
+console.log("printed in the git log");
