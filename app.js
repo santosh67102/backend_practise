9475f647e577a1527d87aea6b9e759a62a6715bf
Santosh Gautam <santoshgautam@Santoshs-MBP.fios-router.home>
2019-04-07 23:34:11 -0400

    add for loop

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-var catme = require('cat-me');
-var jokes = require('knock-knock-jokes');
-var faker = require('faker');
-console.log("******************************************************************************************");
-console.log(catme());
-console.log("******************************************************************************************");
-console.log(catme());
-console.log("******************************************************************************************");
-console.log(catme());
+var express = require("express");
+var app = express();
 
-console.log("******************************************************************************************");
-console.log(faker.address.streetName());
-console.log(faker.name.lastName());
-console.log("******************************************************************************************");
+app.get("/", function(req, res){
+    res.send('Hi this is the Home page ');
+});
+app.get("/bye", function(req, res){
+    res.send('goodbye');
+});
 
-console.log(jokes());
-console.log("******************************************************************************************");
-console.log("****************************THE END****************************");
-console.log("******************************************************************************************");
+
+app.get("/speak/:animal", function(req, res){
+    var sounds = {
+        cow :"moo moo",
+        pig: "oink oink",
+        dog:"woff woff",
+        cat: "meow meow",
+        horse:"hrrff hrrff"
+    }
+    var animal = req.params.animal.toLowerCase();
+    var sound = sounds[animal];
+  
+    res.send("The "+animal+ ' says --> '+ sound);
+    });
+
+app.get("/repeat/:param1/:param2", function(req,res){
+var param1 = req.params.param1;
+var param2= Number(req.params.param2);
+var result = " "; 
+for( var i=0; i<param2; i++){
+        result += (" " + param1);
+};
+res.send(result);
+});
+
+app.get("/*", function(req, res){
+    res.send('ERROR:404 Page cannot be found');
+});
+
+app.listen(3000,function(){
+console.log("SERVER HAS STARTED ON PORT 3000");
+});
