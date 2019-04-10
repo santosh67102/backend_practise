7bbac90e702fbe7572ce277fc759c3d51b44575c
Santosh Gautam <santoshgautam@Santoshs-MBP.fios-router.home>
2019-04-10 00:31:27 -0400

    minor changes

--- more_into_Express/app.js
+++ more_into_Express/app.js
@@ -1,6 +1,7 @@
-var app = require("express")();
+var express = require("express");
+var app = express();
 app.set("view engine", "ejs");
-
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
     res.render("home");
