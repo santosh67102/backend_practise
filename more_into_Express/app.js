314993edd522c72aa5c369e9cbd720a6b1df213d
Santosh Gautam <santoshgautam@Santoshs-MBP.fios-router.home>
2019-04-10 23:25:19 -0400

    included post routes

--- more_into_Express/app.js
+++ more_into_Express/app.js
@@ -1,17 +1,26 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+var friends = ["Bidur","Ballu","dipesh","karun"];
 
 app.get("/",function(req,res){
     res.render("home");
 });
 
 app.get("/friends",function(req,res){
-    var friends = ["Bidur","Ballu","dipesh","karun"];
+
     res.render("friendList", {friends: friends});
 });
 
+app.post("/addFriend",function(req,res){
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
 
 app.get("/love/:thing",function(req,res){
     var loveThing = req.params.thing;
