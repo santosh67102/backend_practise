4858b134c0cbc8acbff54c16c208d34be15eb32c
Santosh Gautam <santoshgautam@Santoshs-MBP.fios-router.home>
2019-04-12 13:39:14 -0400

    april 11

---
+++ APIWork/app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var request = require("request");
+var app = express();
+
+app.get
+
+
+
+
+
+app.listen(3000, function(){
+
+//     console.log("MOVIE APP has Started!!!!! ******");
+// })
+// console.log("making a API request");
+// request("http://google.com", function(error, response, body){
+//     if(!error && response.statusCode==200){
+//         // var parsedBodyData = JSON.parse(body);
+//         console.log(body);
+//     }
+// })
